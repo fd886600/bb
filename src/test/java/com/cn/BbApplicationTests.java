a9750b6cde80f3abc3d2789464f7890d0b3ccc0a
fwf <1563249102@qq.com>
2020-12-01 19:18:42 +0800

    这是Git第一次提交

---
+++ src/test/java/com/cn/BbApplicationTests.java
@@ -0,0 +1,14 @@
+package com.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("初始化Git....");
+    }
+
+}
