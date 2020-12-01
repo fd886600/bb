074320fad85b13bade863251457a5ffab76c7734
fwf <1563249102@qq.com>
2020-12-01 19:36:58 +0800

    这是Git第三次提交

---
+++ src/test/java/com/cn/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("初始化Git....");
+        System.out.println("再次初始化Git....");
+    }
+
+}
